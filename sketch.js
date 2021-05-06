1f6fd8435f561bab023dbd330bd2d8873d06f407
Gunashree S <gunashrees341@gmail.com>
2021-05-06 19:36:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+
+
+
+function setup() {
+  createCanvas(1600, 400);
+
+}
+
+
+function draw() {
+  background(0);  
+  //bullet.sprite.collide(wall.sprite,calculateDeformation)
+  if(hasCollided(bullet, wall))
+  {
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+
+  	
+	if(damage>10)
+	{
+		
+		
+	}
+
+	
+
+	if(damage<10)
+	{
+		
+	}
+	
+  }
+
+
+  drawSprites();
+ 
+}
+
+
+function hasCollided(lbullet, lwall)
+{
+	bulletRightEdge=lbullet.x +lbullet.width;
+	wallLeftEdge=lwall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}
+	return false;
+}
+
+
